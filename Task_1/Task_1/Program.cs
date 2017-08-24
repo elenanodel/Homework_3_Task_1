87074f31df27a48947639b1a0e6866ab37fd4464
Леон <@->
2017-08-24 16:25:08 +0300

    Init Commit

---
+++ Task_1/Task_1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 100;
+            int b = 1000;
+            int c;
+
+            Console.Write("Create a new price: ");
+            c = int.Parse(Console.ReadLine());
+
+            if (c < a)
+            {
+                Console.WriteLine("Your price is lesser than the minimum price. You should to contact with trader!");
+            }
+            if (c > b)
+            {
+                Console.WriteLine("Your price is bigger than the maximum price. You should to contact with trader!");
+            }
+            else if (a >= c && b <= c)
+            {
+                Console.WriteLine("The new price was created");
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
